aa4aab0c976f468d063b84b37dfc268ca96045df
mbenitez <mbenitez.a@gmail.com>
2022-11-13 19:20:37 -0500

    Properly format notes saved to db.json. Display new note

--- server.js
+++ server.js
@@ -17,7 +17,15 @@ app.use(express.static('public'));
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
-app.get('/api/notes', (req, res) => res.json(noteData));
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err,data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });
+});
 
 app.post('/api/notes', (req, res) => {
     console.info(`${req.method} request to create a note`);
@@ -27,30 +35,26 @@ app.post('/api/notes', (req, res) => {
         text,
         note_id: uuidv4()
     };
-    console.log(newNote);
-    // Convert the data to a string
-    const noteString = JSON.stringify(newNote, null, '\t\t');
-    console.log("noteString: " + noteString )
-    writeNote(newNote, noteString);
+    //console.log(newNote);
+    writeNote(newNote);
+    const response = {
+        status: 'Success',
+        body: newNote,
+    };
+    res.status(201).json(response);
 });
 
-function writeNote (newNote, noteString){
+function writeNote (newNote){
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
             console.log(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(newNote);
+            fs.writeFile('./db/db.json', JSON.stringify(parsedData, null, 4), (err) =>
+            err ? console.error(err) : console.info(`\nData written to db.json`)
+          );
         }
-        // Remove last two lines up to '}'
-        let linesExceptLast = data.split('\n').slice(0,-3).join('\n');
-        // Add '},' to append new json element
-        let newLines = linesExceptLast + '\n\t},\n\t' ;
-        console.log("New lines \n" + newLines);
-        fs.writeFile('./db/db.json', newLines + noteString + '\n]\n',(err) =>
-            err ? 
-            console.error(err) : 
-            console.log(`New note ${newNote.title} has been saved`)
-        );
-        //fs.appendFile(noteString);
-        //fs.appendFile(newNote + '\n]\n');
     });
 }
 
